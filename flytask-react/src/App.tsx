cae40e6104b523fc7ecdcd8a5be8f73651e9af88
000050519 <00050519@uca.edu.sv>
2024-04-24 10:36:28 -0600

    Basic Authentication

--- flytask-react/src/App.tsx
+++ flytask-react/src/App.tsx
@@ -1,7 +1,3 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import "./App.css";
 import LoginPage from "./pages/Sign-In";
 import SignupPage from "./pages/Sign-Up";
