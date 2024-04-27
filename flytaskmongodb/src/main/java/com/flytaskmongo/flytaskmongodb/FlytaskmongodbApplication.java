80c8b8d68480fc549cbbff009029c62a9bd1d22c
enriquegarcia92 <00093619@uca.edu.sv>
2024-04-27 08:51:47 -0600

    Inicialización de proyecto mongodb

---
+++ flytaskmongodb/src/main/java/com/flytaskmongo/flytaskmongodb/FlytaskmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.flytaskmongo.flytaskmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlytaskmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlytaskmongodbApplication.class, args);
+	}
+
+}
---
+++ flytaskmongodb/src/test/java/com/flytaskmongo/flytaskmongodb/FlytaskmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flytaskmongo.flytaskmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlytaskmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
