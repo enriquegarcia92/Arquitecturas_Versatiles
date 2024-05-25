685531a167c2190d9d22f506a20fba6da9e8800c
andreayalad <alejandrayala01@gmail.com>
2024-05-25 11:18:54 -0600

    Edit: User entity

--- flytaskexpressmongo/src/entities/User.ts
+++ flytaskexpressmongo/src/entities/User.ts
@@ -6,13 +6,16 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  role: string;
 }
 
 const userSchema = new Schema<IUser>({
   _id: { type: Number, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String, required: true }
+  
 });
 
 userSchema.pre<IUser>('save', async function (next) {
