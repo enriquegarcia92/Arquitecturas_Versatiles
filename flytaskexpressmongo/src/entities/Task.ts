8b1ccb9e3824e4413709b751d5d70da3f6977e1a
andreayalad <alejandrayala01@gmail.com>
2024-06-01 10:44:19 -0600

    Add and Edit: Task Controller methods and editing Task entity for export

--- flytaskexpressmongo/src/entities/Task.ts
+++ flytaskexpressmongo/src/entities/Task.ts
@@ -11,3 +11,29 @@ export interface ITask extends Document {
     userId: number;
     _class?: string;
 }
+const taskSchema = new Schema<ITask>({
+    _id: { type: Number, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: Number, required: true },
+    creationDate: { type: Date, required: true },
+    dueDate: { type: Date, required: true },
+    userId: { type: Number, required: true },
+    _class: { type: String }
+});
+
+taskSchema.pre<ITask>('save', async function(next) {
+    if (this.isNew) {
+        try {
+            this._id = await getNextSequenceValue('task_sequence');
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next();
+    }
+});
+
+const Task = model<ITask>('Task', taskSchema);
+export default Task;
