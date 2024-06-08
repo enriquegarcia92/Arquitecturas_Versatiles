706b3f00088b1b87503a1590f01eebcb5727938b
andreayalad <alejandrayala01@gmail.com>
2024-06-08 11:00:29 -0600

    Add: New topbar and Kanit Font in CSS file

--- arquitecturas_integrales/src/App.jsx
+++ arquitecturas_integrales/src/App.jsx
@@ -1,15 +1,16 @@
 // src/App.jsx
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Content from './components/Content';
+import Topbar from './components/Topbar';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col h-screen">
-        <Header />
+        <Topbar />
         <div className="flex flex-grow">
           <Sidebar />
           <Content />
