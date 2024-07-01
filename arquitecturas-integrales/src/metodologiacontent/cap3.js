7ed95192d859632d448ada0a0e40920b16d3a4b9
00091318 <00091318uca.edu.sv>
2024-06-30 22:22:44 -0600

    agregando nuevo js cap3

---
+++ arquitecturas-integrales/src/metodologiacontent/cap3.js
@@ -0,0 +1,125 @@
+export const analisis = `Análisis de requerimientos de la aplicación a desarrollar
+La fase de levantamiento de requerimientos, comprende la base del proyecto en la cual se debe identificar, comprender y documentar las necesidades de los usuarios, las funcionalidades de la aplicación y las características técnicas que debe cumplir. Para esta fase es necesario realizar las siguientes sub-fases:`;
+
+export const analisis1 = `Comprender las necesidades de los usuarios.`;
+
+export const analisis2 = `Definir funcionalidades del sistema.`;
+
+export const analisis3 = `Especificar las características técnicas (definición de diagrama de clases).`;
+
+export const diseño = `Diseño
+Los requerimientos y funcionalidades de la aplicación definidos son base para el flujo de la aplicación, la cual se diseña gráficamente antes de iniciar con la programación. Existen diferentes herramientas que permiten el diseño de mockup estáticos como interactivos un ejemplo figma. Para esta fase es necesario realizar la siguiente sub-fase:`;
+
+export const diseño1 = `Desarrollo de mockup.`;
+
+export const diseño2 = `Requerimientos técnicos de hardware y software.`;
+
+export const hardware = `Requerimiento de hardware:
+Procesador: 2.5 GHz o superior.
+Memoria RAM: 16 GB mínimo.
+Espacio en disco: 256 GB SSD mínimo.
+Sistema operativo: Windows 10 64-bit (build 19043 o posterior), macOS 10.10 o posterior, o Ubuntu 18.04 o posterior.`;
+
+export const software = `Requerimiento de software:
+IDE: Visual Studio Code.
+Control de versiones: Git.
+Frameworks: Angular, React, Spring Boot, Django, Express.js, Vue.js.
+Bases de datos: MongoDB, PostgreSQL.
+Contenedores: Docker, Docker Compose.
+Servidor web: Nginx.
+Estas herramientas son comúnmente utilizadas en el desarrollo de aplicaciones modernas y proporcionan un entorno consistente y controlado para el desarrollo y la implementación de la aplicación.`;
+
+export const arquitectura = `Diseño de la Arquitectura de la aplicación
+Para el desarrollo de aplicaciones basadas en una arquitectura Cliente-Servidor se facilita el manejo de las tecnologías de lógica de negocios y la interfaz al usuario ya que se comunican de manera efectiva para establecer el flujo de datos necesario para el funcionamiento de una aplicación. Se elige este tipo de arquitectura específicamente porque permite que mediante el protocolo HTTP y el formato JSON que todas las aplicaciones implementadas pueden ser consumidas por todos los clientes, lo que hace que desde cualquier parte que se requiera la lógica de negocios esta puede ser consultada mediante internet. Para esta fase es necesario realizar las siguientes subfases:`;
+
+export const arquitectura1 = `Selección de tecnologías para el Front End.`;
+
+export const arquitectura2 = `Selección de tecnologías para el Back End.`;
+
+export const arquitectura3 = `Selección de bases de datos.`;
+
+export const patrones = `Patrones de diseño
+Facilita la mantenibilidad y manipulación del código, permite escalabilidad, separar las responsabilidades y por ende facilita encontrar errores y corregirlos. Según el programador puede variar el patrón de diseño implementado, además de que va sujeto a cómo funciona la tecnología. Para esta fase es necesario realizar la siguiente sub-fase:`;
+
+export const patrones1 = `Selección de patrones de diseño.`;
+
+export const buenasPracticas = `Buenas prácticas de programación
+Conjunto de prácticas permite mantener un flujo de trabajo organizado y garantizar la calidad de la aplicación. Algunas prácticas tienen su origen en patrones conocidos de desarrollo para las distintas capas del sistema que permiten crear aplicaciones con alta experiencia de calidad para el usuario y código de fácil mantenimiento y escalabilidad para los desarrolladores. Para esta fase es necesario realizar la siguiente sub-fase:`;
+
+export const buenasPracticas1 = `Identificar algunas de las buenas prácticas actuales para un desarrollo full stack.`;
+
+export const seguridad = `Buenas prácticas de seguridad
+La seguridad para un proyecto es de vital importancia, no es posible proveer al usuario una experiencia de alta calidad al utilizar la aplicación si no se puede garantizar la seguridad de los datos y del sistema en sí. Para esta fase es necesario realizar las siguientes sub-fases:`;
+
+export const seguridad1 = `Identificar algunas buenas prácticas de seguridad, para el acceso remoto a los servidores.`;
+
+export const seguridad2 = `Controlar el flujo de tráfico entre los clientes y servidores.`;
+
+export const seguridad3 = `Manejo y autenticación de usuarios en la aplicación.
+A continuación, listan algunos ejemplos de tecnologías más utilizadas en el desarrollo de aplicaciones:`;
+
+export const tecnologia1 = `SSH (Secure Shell): permite el acceso remoto seguro a los servidores. Es útil cuando se requiere administrar servidores remotos o transferir archivos de manera segura.`;
+
+export const tecnologia2 = `Reverse Proxy: permite controlar y optimizar el flujo de tráfico entre los clientes y los servidores. Es útil para balancear la carga, proporcionar seguridad adicional y mejorar la velocidad y eficiencia de las aplicaciones web.`;
+
+export const tecnologia3 = `NoSQL: permite trabajar con grandes volúmenes de datos distribuidos. Este es utilizado cuando el volumen de los datos no se ajusta a un esquema rígido, se necesitan escalabilidad horizontal y alta disponibilidad, o se requiere un rendimiento rápido con grandes volúmenes de datos (IBM, 2022).`;
+
+export const tecnologia4 = `JWT (JSON Web Token): permite manejar la autenticación y autorización de usuarios en la aplicación. Es útil cuando se requiere una forma segura de transmitir información entre partes como la identidad del usuario, los roles y los privilegios (España, 2020).`;
+
+export const programacion = `Programación
+Para esta fase es necesario realizar las siguientes subfases:`;
+
+export const programacion1 = `Implementación de buenas prácticas de codificación.`;
+
+export const programacion2 = `Implementación de los patrones de diseño.`;
+
+export const programacion3 = `Implementación de buenas prácticas de seguridad con JWT.`;
+
+export const programacion4 = `Desarrollo del Front End.`;
+
+export const programacion5 = `Desarrollo del Back End.`;
+
+export const programacion6 = `Integración del Back End y Front End.`;
+
+export const pruebas = `Pruebas
+Para esta fase es necesario realizar la siguiente sub-fase:`;
+
+export const pruebas1 = `Pruebas unitarias: verifican la precisión de un bloque de código aislado.`;
+
+export const pruebas2 = `Pruebas de integración: evalúan cómo interactúan varios módulos de software.`;
+
+export const pruebas3 = `Pruebas de aceptación: verifican si un sistema cumple con los requisitos empresariales.`;
+
+export const despliegue = `Despliegue
+Es la etapa esencial en el desarrollo de software que implica la transición de aplicaciones a producción.`;
+
+export const despliegue1 = `Selección de tecnologías para contenedores para el despliegue: a continuación, se listan algunas ventajas que se adquieren al implementar los contenedores:`;
+
+export const contenedores1 = `Aislamiento: Las aplicaciones se ejecutan en entornos aislados, evitando conflictos y problemas de compatibilidad.`;
+
+export const contenedores2 = `Portabilidad: Se pueden implementar en diferentes entornos, desde servidores locales hasta nubes públicas.`;
+
+export const contenedores3 = `Escalabilidad: Se pueden escalar fácilmente para satisfacer las demandas cambiantes de tráfico y carga de trabajo.`;
+
+export const contenedores4 = `Agilidad: Agilizan el proceso de despliegue, ya que no requieren la instalación de un sistema operativo completo para cada aplicación.`;
+
+export const servidorWeb = `Selección de Servidor web: permite a la aplicación obtener los siguientes beneficios:`;
+
+export const servidorWeb1 = `Infraestructura confiable: permite garantizar la disponibilidad y el rendimiento óptimos de la aplicación.`;
+
+export const servidorWeb2 = `Conectividad de alta velocidad: permite garantizar un acceso rápido y fluido a la aplicación por parte de los usuarios.`;
+
+export const servidorWeb3 = `Seguridad sólida: permite proteger la aplicación y los datos de los usuarios contra amenazas cibernéticas.`;
+
+export const servidorWeb4 = `Escalabilidad: permite que la infraestructura pueda adaptarse al crecimiento de la aplicación y la demanda de los usuarios.`;
+
+export const ciCd = `Herramientas de integración continua y entrega continua (CI/CD)
+La implementación de estas herramientas permitirá a la aplicación contar con las siguientes características:`;
+
+export const ciCd1 = `Mayor velocidad: Automatizan tareas repetitivas, permitiendo a los equipos desarrollar y entregar software con mayor rapidez.`;
+
+export const ciCd2 = `Mejor calidad: La automatización de las pruebas ayuda a identificar y corregir errores antes de que se implementen en producción.`;
+
+export const ciCd3 = `Mayor confiabilidad: Garantizan un proceso de despliegue consistente y confiable, lo que reduce el riesgo de errores y fallos.`;
+
+export const ciCd4 = `Mayor colaboración: Facilitan la colaboración entre equipos de desarrollo y operaciones.`;
--- arquitecturas-integrales/src/pages/subPages/analisisreq/anareqcomponent.jsx
+++ arquitecturas-integrales/src/pages/subPages/analisisreq/anareqcomponent.jsx
@@ -1,8 +1,17 @@
 import react from "react"
+import TextBlock from "../../../components/TextBlock"
+import CodeBlock from "../../../components/CodeBlock"
+import { analisis, analisis1, analisis2, analisis3} from "../../../metodologiacontent/cap3";
 
 const Anareqcomponent = () => {
     return (
     < div className="flex flex-col">
+
+<TextBlock textContent={analisis} />
+<TextBlock textContent={analisis1} />
+<TextBlock textContent={analisis2} />
+<TextBlock textContent={analisis3} />
+
     </div>
     );
 };
